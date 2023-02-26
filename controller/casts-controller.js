cf3d69244eeeb31c9422155d1ee5c9268cd82514
NHWai <nayhtetwinee@gmail.com>
2023-02-26 20:52:22 +0630

    currying the controller funcs

--- controller/casts-controller.js
+++ controller/casts-controller.js
@@ -1,85 +1,88 @@
 const CastService = require("../service/CastService");
 
-const getAllCasts = async (req, res, next) => {
-  try {
-    const castList = await CastService.getAllCasts();
-    if (!castList) throw Error("Cannot get casts lists");
-    res.status(200).json(castList);
-  } catch (err) {
+//Error Handler Function
+const handle = (fn, httpErrorCode) => (req, res, next) => {
+  return fn(req, res, next).catch((err) =>
     res
-      .status(400)
-      .json({ Error: Object.keys(err).length === 0 ? err.message : err });
-  }
+      .status(httpErrorCode)
+      .json({ Error: Object.keys(err).length === 0 ? err.message : err })
+  );
 };
 
-const newCast = async (req, res, next) => {
-  const cast = req.body;
-  try {
-    const newCast = await CastService.saveCast(cast);
-    if (!newCast) throw Error("Cannot create a new cast");
-    res.status(201).json(newCast);
-  } catch (err) {
-    res
-      .status(400)
-      .json({ Error: Object.keys(err).length === 0 ? err.message : err });
-  }
+// Handler Function for each HTTP Verbs
+const getAllCastsHandler = async (req, res, next) => {
+  const casts = await CastService.getAllCasts();
+  if (!casts) throw Error("No Casts found");
+  return res.status(200).json(casts);
 };
 
-const getCastsByMovieId = async (req, res, next) => {
+const getCastsByMovieIdHandler = async (req, res, next) => {
   const movieId = req.params["movieId"];
-  console.log("movieId =>", movieId);
-  try {
-    const movie = await CastService.getCastsByMovieId(movieId);
-    console.log("movies =>", movie);
-    if (!movie) throw Error("Cannot get casts with given movieId");
-    res.status(200).json(movie);
-  } catch (err) {
-    res
-      .status(400)
-      .json({ Error: Object.keys(err).length === 0 ? err.message : err });
-  }
+  const movie = await CastService.getCastsByMovieId(movieId);
+  if (!movie) throw Error("Cannot get casts with given movieId");
+  return res.status(200).json(movie);
 };
 
-const getCastById = async (req, res, next) => {
+const getCastByIdHandler = async (req, res, next) => {
   const castId = req.params["castId"];
-  try {
-    const cast = await CastService.getCastById(castId);
-    if (!cast) throw Error("No casts found for given Id");
-    res.status(200).json(cast);
-  } catch (err) {
-    res
-      .status(400)
-      .json({ Error: Object.keys(err).length === 0 ? err.message : err });
-  }
+  const cast = await CastService.getCastById(castId);
+  if (!cast) throw Error("No casts found for given Id");
+  return res.status(200).json(cast);
 };
 
-const updateCast = async (req, res, next) => {
+const newCastHandler = async (req, res, next) => {
+  const cast = req.body;
+  const newCast = await CastService.saveCast(cast);
+  if (!newCast) throw Error("Cannot create a new cast");
+  return res.status(201).json(newCast);
+};
+
+const updateCastHandler = async (req, res, next) => {
   const castId = req.params["castId"];
   const cast = req.body;
-  try {
-    const updatedCast = await CastService.updateCast(castId, cast);
-    if (!updatedCast) throw Error("Cannot update the cast");
-    res.status(200).json(updatedCast);
-  } catch (err) {
-    res
-      .status(400)
-      .json({ Error: Object.keys(err).length === 0 ? err.message : err });
-  }
+  const updatedCast = await CastService.updateCast(castId, cast);
+  if (!updatedCast) throw Error("Cannot update the cast");
+  return res.status(200).json(updatedCast);
 };
 
-const deleteCast = async (req, res, next) => {
+const deleteCastHandler = async (req, res, next) => {
   const castId = req.params["castId"];
-  console.log("castId =>", castId);
-  try {
-    const deleted = await CastService.deleteCast(castId);
-    if (!deleted) throw Error("Cannot delete the cast of given Id");
-    res.status(204).end();
-  } catch (err) {
-    res
-      .status(400)
-      .json({ Error: Object.keys(err).length === 0 ? err.message : err });
-  }
+  const deleted = await CastService.deleteCast(castId);
+  if (!deleted) throw Error("Cannot delete the cast of given Id");
+  res.status(204).end();
 };
+
+//Mapping the related function for each HTTP Verbs
+const getAllCasts = (req, res, next) =>
+  handle(getAllCastsHandler, 400)(req, res, next);
+
+const getCastsByMovieId = (req, res, next) =>
+  handle(getCastsByMovieIdHandler, 400)(req, res, next);
+
+const getCastById = (req, res, next) =>
+  handle(getCastByIdHandler, 400)(req, res, next);
+
+const newCast = (req, res, next) => handle(newCastHandler, 400)(req, res, next);
+
+const updateCast = (req, res, next) =>
+  handle(updateCastHandler, 400)(req, res, next);
+
+const deleteCast = (req, res, next) =>
+  handle(deleteCastHandler, 400)(req, res, next);
+
+// const deleteCast = async (req, res, next) => {
+//   const castId = req.params["castId"];
+//   console.log("castId =>", castId);
+//   try {
+//     const deleted = await CastService.deleteCast(castId);
+//     if (!deleted) throw Error("Cannot delete the cast of given Id");
+//     res.status(204).end();
+//   } catch (err) {
+//     res
+//       .status(400)
+//       .json({ Error: Object.keys(err).length === 0 ? err.message : err });
+//   }
+// };
 
 module.exports = {
   getAllCasts,
--- controller/movies-controller.js
+++ controller/movies-controller.js
@@ -1,4 +1,3 @@
-const { find } = require("../models/Casts");
 const MovieService = require("../service/MovieService");
 
 //Error Handler Function
@@ -11,41 +10,43 @@ const handle = (fn, httpErrorCode) => (req, res, next) => {
 };
 
 // Handler Function for each HTTP Verbs
-const getAllMoviehandler = async (req, res, next) => {
+const getAllMovieHandler = async (req, res, next) => {
   const movielist = await MovieService.getAllMovies();
   if (!movielist) throw Error("No movies");
   return res.status(200).json(movielist);
 };
 
-const getMovieByIdhandler = async (req, res, next) => {
+const getMovieByIdHandler = async (req, res, next) => {
   const movieId = req.params["movieId"];
   const movie = await MovieService.getMovieById(movieId);
   if (!movie) throw Error("No movies found for given id");
   return res.status(200).json(movie);
 };
 
-const findMovieByTitlehandler = async (req, res, next) => {
+const findMovieByTitleHandler = async (req, res, next) => {
   const title = req.params["title"].split("_").join(" ");
   const movie = await MovieService.getMovieByTitle(title);
   if (!movie) throw Error("No movies found by given title");
   return res.status(200).json(movie);
 };
 
-const newMoviehandler = async (req, res, next) => {
+const newMovieHandler = async (req, res, next) => {
   const movie = req.body;
   const newMovie = await MovieService.saveMovie(movie);
   if (!newMovie) throw Error("Cannot create a new movie");
   return res.status(200).json(newMovie);
 };
 
-const findMovieByDirectorhandler = async (req, res, next) => {
+const findMovieByDirectorHandler = async (req, res, next) => {
   const director = req.params["director"];
+
   const movies = await MovieService.findMovieByDirector(director);
-  if (!movies) throw Error("No movies Found");
+
+  if (!movies || movies.length === 0) throw Error("No movies Found");
   return res.status(200).json(movies);
 };
 
-const updateMoviehandler = async (req, res, next) => {
+const updateMovieHandler = async (req, res, next) => {
   const movieId = req.params["movieId"];
   const movie = req.body;
   const movieUpdated = await MovieService.updateMovie(movieId, movie);
@@ -53,7 +54,7 @@ const updateMoviehandler = async (req, res, next) => {
   return res.status(200).json(movieUpdated);
 };
 
-const deleteMoviehandler = async (req, res, next) => {
+const deleteMovieHandler = async (req, res, next) => {
   const movieId = req.params["movieId"];
   const movie = await MovieService.deleteMovie(movieId);
   return res.status(204).json(movie);
@@ -62,25 +63,25 @@ const deleteMoviehandler = async (req, res, next) => {
 //Mapping the related function for each HTTP Verbs
 
 const getAllMovie = (req, res, next) =>
-  handle(getAllMoviehandler, 400)(req, res, next);
+  handle(getAllMovieHandler, 400)(req, res, next);
 
 const getMovieById = (req, res, next) =>
-  handle(getMovieByIdhandler, 400)(req, res, next);
+  handle(getMovieByIdHandler, 400)(req, res, next);
 
 const findMovieByTitle = (req, res, next) =>
-  handle(findMovieByTitlehandler, 400)(req, res, next);
+  handle(findMovieByTitleHandler, 400)(req, res, next);
 
 const findMovieByDirector = (req, res, next) =>
-  handle(findMovieByDirectorhandler, 400)(req, res, next);
+  handle(findMovieByDirectorHandler, 400)(req, res, next);
 
 const newMovie = (req, res, next) =>
-  handle(newMoviehandler, 400)(req, res, next);
+  handle(newMovieHandler, 400)(req, res, next);
 
 const updateMovie = (req, res, next) =>
-  handle(updateMoviehandler, 400)(req, res, next);
+  handle(updateMovieHandler, 400)(req, res, next);
 
 const deleteMovie = (req, res, next) =>
-  handle(deleteMoviehandler, 400)(req, res, next);
+  handle(deleteMovieHandler, 400)(req, res, next);
 
 module.exports = {
   getAllMovie,
